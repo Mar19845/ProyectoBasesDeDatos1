42f8ffa8f0447b7889715cb160345996adae31fd
Mar19845 <54874288+Mar19845@users.noreply.github.com>
2021-03-08 15:11:28 -0600

    se agregaron metodos y se conecta a una bases de datos

--- server.js
+++ server.js
@@ -1,22 +1,38 @@
-const express = require ("express")
+const express = require("express")
 const app = express();
-app.use(express.json())
+//cors, permite que se puedan hacer get/post desde cualquier pagina
+var cors = require('cors')
+app.use(express.json(),cors())
 
 //conecion a postgresql
 const { Pool } = require('pg')
 const pool = new Pool({
-    user: '{user}',
-    host: '{host}',
-    database: '{database}',
-    password: '{password}',
-    port: 5432,
-  })
-
+  user: 'postgres',
+  host: 'localhost',
+  database: 'Lab05',
+  password: '1234',
+  port: 5432,
+})
+//si entran al local host se desplega la pagina, index http://localhost:8080/
+app.get("/", (req, res) => res.sendFile(`${__dirname}/index.html`))
 //get del servidor
 app.get("/get", async (req, res) => {
-    console.log("el get funciona")
-    res.send("Esto funcionaaaaaaaaaaaaaaaaa perrrooooos")
+  const rows = await readTodos();
+  console.log("el get funciona")
+  //res.send("Esto funcionaaaaaaaaaaaaaaaaa perrrooooos")
+  res.setHeader("content-type", "application/json")
+  res.send(JSON.stringify(rows))
 })
 // pone al servidor a esuchar en el puerto 8080
 app.listen(8080, () => console.log("Web server is listening.. on port 8080"))
 
+
+async function readTodos() {
+  try {
+    const results = await pool.query("select * from carros");
+    return results.rows;
+  }
+  catch (e) {
+    return [];
+  }
+}
