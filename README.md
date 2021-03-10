6a6b02799ce78d507175c49ed222754f32cc6cf3
Mar19845 <54874288+Mar19845@users.noreply.github.com>
2021-03-09 19:43:24 -0600

    Se agregaron cambios respecto al server.js

--- README.md
+++ README.md
@@ -3,5 +3,14 @@
 Integrantes:
 
 Juan Marroquin 19845
-Javier Cotto 19
-Javier Hernandez 19
+Javier Cotto 19324
+Javier Hernandez 19202
+
+Requisitos para el funcionamiento del Server.
+
+Tener instalado node, posteriormente intstallar los siguientes dependencias
+
+Express -----   npm install express 
+cors  -----     npm install cors
+pg  -----      npm install pg
+dotenv -----   npm install dotenv
--- server.js
+++ server.js
@@ -2,24 +2,28 @@ const express = require("express")
 const app = express();
 //cors, permite que se puedan hacer get/post desde cualquier pagina
 var cors = require('cors')
-app.use(express.json(),cors())
+app.use(express.json(), cors())
+
+
+//dot env para el manejo de links
+require('dotenv').config()
+//link para conectarse a la base de datos en el servidor
+const connectionString = process.env.URL
+
 
 //conecion a postgresql
 const { Pool } = require('pg')
+
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'Lab05',
-  password: '1234',
-  port: 5432,
+  connectionString,
 })
+
 //si entran al local host se desplega la pagina, index http://localhost:8080/
 app.get("/", (req, res) => res.sendFile(`${__dirname}/index.html`))
-//get del servidor
+//un entry point del servidor
 app.get("/get", async (req, res) => {
   const rows = await readTodos();
   console.log("el get funciona")
-  //res.send("Esto funcionaaaaaaaaaaaaaaaaa perrrooooos")
   res.setHeader("content-type", "application/json")
   res.send(JSON.stringify(rows))
 })
@@ -29,7 +33,7 @@ app.listen(8080, () => console.log("Web server is listening.. on port 8080"))
 
 async function readTodos() {
   try {
-    const results = await pool.query("select * from carros");
+    const results = await pool.query("select * from prueba");
     return results.rows;
   }
   catch (e) {
