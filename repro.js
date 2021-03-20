479a8830a605b3f29e7834af305b61c5cfe88b61
JavierCotto5201 <53280525+JavierCotto5201@users.noreply.github.com>
2021-03-20 15:27:09 -0600

    Paginas

---
+++ repro.js
@@ -0,0 +1,16 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
+
+var input = document.getElementById("Asearch");
+
+input.addEventListener("keyup", function(event) {
+
+  if (event.keyCode === 13) {
+    //AGREGAR FUNCION
+  }
+});
